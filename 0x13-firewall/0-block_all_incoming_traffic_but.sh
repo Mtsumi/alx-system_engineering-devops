26b1705a789b86167eb40bc360216ad3824209b5
Mtsumi <mtsumijr@gmail.com>
2022-10-11 16:46:13 +0300

    firewall success

--- 0x13-firewall/0-block_all_incoming_traffic_but.sh
+++ 0x13-firewall/0-block_all_incoming_traffic_but.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt-get install ufw
+sudo apt-get update
+sudo apt-get install ufw -y
 sudo ufw status
 sudo ufw enable -y
+
 sudo ufw default deny incoming -y
 sudo ufw default allow outgoing -y
 sudo ufw allow 22/tcp
 sudo ufw allow 443/tcp
 sudo ufw allow 80/tcp
+
---
+++ 0x13-firewall/README.md
@@ -0,0 +1,4 @@
+Your servers without a firewall…
+
+![Alt text](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-sysadmin_devops/155/holbertonschool-firewall.gif)
+
